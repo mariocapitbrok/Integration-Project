e8ef4fedb7613f4fc35130d51cf68b507e18f10f
Mario Capitbrok <mario@capitbrok.com>
2023-10-09 19:22:30 +0000

    Update authorization URL

--- src/services/asanaService.ts
+++ src/services/asanaService.ts
@@ -1,12 +1,25 @@
-const axios = require('axios');
-const { PrismaClient } = require('@primsa/client');
+import axios from 'axios';
+import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 function generateAuthorizationURL() {
-    const clientId = ''
-    const redirectUri = ''
-    const scope = ''
-    const authorizationUrl = ``
+  const clientId = process.env.CLIENT_ID;
+  const redirectUri = process.env.REDIRECT_ID;
 
-    return authorizationUrl
+  const responseType = 'code';
+  const state = 'thisIsARandomString';
+  const scope = 'default';
+
+  const authorizationUrl = `https://app.asana.com/-/oauth_authorize
+    ?client_id=${clientId}
+    &redirect_uri=${redirectUri}
+    &response_type=${responseType}
+    &state=${state}
+    &scope=${scope}`;
+
+  return authorizationUrl;
 }
