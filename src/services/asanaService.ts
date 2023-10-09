c028ca2a8012d07db8705668c416fb9c5b1cc675
Mario Capitbrok <mario@capitbrok.com>
2023-10-09 18:45:53 +0000

    Create generateAuthorizationURL function

---
+++ src/services/asanaService.ts
@@ -0,0 +1,12 @@
+const axios = require('axios');
+const { PrismaClient } = require('@primsa/client');
+const prisma = new PrismaClient();
+
+function generateAuthorizationURL() {
+    const clientId = ''
+    const redirectUri = ''
+    const scope = ''
+    const authorizationUrl = ``
+
+    return authorizationUrl
+}
