c47865ee24b172423daf7c9a2de4ae52ebe331d9
Mario Capitbrok <mario@capitbrok.com>
2023-10-10 04:06:04 +0000

    Refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 require('dotenv').config();
 import express from 'express';
-import { GoogleService } from 'services';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
 import { v4 as uuidv4 } from 'uuid';
+import cookieParser from 'cookie-parser';
+import { GoogleService } from 'services';
 import { AsanaService } from 'services';
-import { handleOAuthCallback } from 'services/asana';
 
 const app = express();
 const port = 8080;
@@ -53,7 +52,7 @@ app.get('/asana/callback', async (req, res) => {
   console.log(`code: ${req.query.code}`);
   console.log(`state: ${req.query.state}\n`);
 
-  const response = await handleOAuthCallback(req.query.code);
+  const response = await AsanaService.handleOAuthCallback(req.query.code);
 
   res.redirect(`/?access_token=${response.data.access_token}`);
 });
